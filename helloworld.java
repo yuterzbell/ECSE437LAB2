f2d4d4521199da9d6555df1d8130814be14ac908
yuterzbell <yuta.bellali@mail.mcgill.ca>
2022-09-20 15:07:12 -0400

    first version of my code
    Change-Id: Ic9c7cb895c04db62751bf597bc1b2e3386159b0f

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.out);
+    System.out.print("Input your first: ");
+    String fname = input.next();
+
+    System.out.print("Input your last name: ");
+    String lname = input.next();
+
+    System.out.println();
+    System.out.println("Hello \n"+fnames+" "+lnames)
+  }
+}
